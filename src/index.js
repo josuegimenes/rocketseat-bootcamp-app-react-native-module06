bb4d572b3a0a87d2f046e34dc7032d4e8ee32dae
Josué Gimenes <josuegalvares@gmail.com>
2020-01-16 16:20:05 -0300

    Sending push notifications

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
-const App = () => (
-  <>
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Routes />
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('2b768bd1-1d4d-42c7-8c1f-fe1bbadd91d5');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </>
+    );
+  }
+}
 
 export default CodePush({
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
